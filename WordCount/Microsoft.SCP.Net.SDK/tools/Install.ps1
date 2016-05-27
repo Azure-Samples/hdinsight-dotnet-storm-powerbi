8474593e2be6e52c8a84633e2a0570f5f884c204
Larry Franks <larry.franks@microsoft.com>
2016-05-27 16:13:32 -0400

    Refactoring to use Storm > SQL Database > Power BI as the data flow

--- WordCount/Microsoft.SCP.Net.SDK/tools/Install.ps1
+++
@@ -1,6 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-$scpHostConfig = $project.ProjectItems.Item("SCPHost.exe.config")
- 
-# set 'Copy To Output Directory' to 'Copy always'
-$scpHostConfigCopyAttribute = $scpHostConfig.Properties.Item("CopyToOutputDirectory")
-$scpHostConfigCopyAttribute.Value = 1
